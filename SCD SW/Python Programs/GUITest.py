92fb1365b639d5ea6c25f31ff3b4b57b1f08fc26
Jacobcok <jacobcok@gmail.com>
2018-04-25 13:19:10 -0400

    added duration GUI control

--- SCD SW/Python Programs/GUITest.py
+++ SCD SW/Python Programs/GUITest.py
@@ -9,15 +9,27 @@ temp = "0"
 name = ''
 
 # serial for linux
-ser = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=0)
+# ser = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=0)
 
 # enables gui window
 master = Tk()
-master.wm_title("title goes here")
+master.wm_title("T-Test")
 
 # make a entry box 
-log = Entry(master, width=50, takefocus=0)
+logLabelText = StringVar()
+logLabelText.set("Save Name: ")
+logLabel = Label(textvariable=logLabelText)
+logLabel.pack()
+log = Entry(master, width=30, takefocus=1)
 log.pack()
+
+# duration text box
+durationBoxLabelText = StringVar()
+durationBoxLabelText.set("Set Duration: ")
+durationBoxLabel = Label(textvariable=durationBoxLabelText)
+durationBoxLabel.pack()
+durationBox = Entry(master, width=30, takefocus=1)
+durationBox.pack()
 
 # store textfield to variable 'name'
 def saveName():
@@ -27,10 +39,12 @@ def saveName():
 # save data to a file using 'name'
 def printText():
 	global t_end, temp, name, ser, f, startTime, time, master
+	name = log.get()
+	runTime = int(durationBox.get())
 	# find the time when button call started
 	startTime = time.time()
 	# find the time for how long the duration of the test
-	t_end = startTime + 5
+	t_end = startTime + runTime
 	# file name, appending mode	
 	f = open(name,'a')
 	print('start recording serial')
@@ -45,11 +59,11 @@ def printText():
 		percentComplete = 100*numer/denom	
 		update_progress(percentComplete)
 
-		temp = ser.read()
+		temp = "\n" # ser.read()
 		# write serial to 'name' unless end of line or carridge return
 		while (temp != "\n") and (temp != "\r"):
 			f.write(temp)
-			temp = ser.read()
+			temp = "temp data" # ser.read()
 		# if end of line append comma seperation and timestamp to 'name'
 		if temp == "\n":
 			f.write(",")
@@ -64,11 +78,13 @@ def update_progress(progress):
 
 
 # create button 
-B = Button(master, text="Set Save Name", width=11, command=saveName)
-B.pack(side=RIGHT)
+# B = Button(master, text="Set Save Name", width=11, command=saveName)
+# B.pack(side=RIGHT)
 C = Button(master, text="Run Test", width=11, command=printText)
-C.pack(side=LEFT)
+C.pack()
 
 mainloop()
-log.Entry(master, width=50)
+log.Entry(master, width=50, takefocus=1)
 log.pack()
+durationBox.Entry(master, width=50)
+durationBox.pack()
